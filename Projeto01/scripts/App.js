19d78c7afa3601a8469f2558faa392e1d6707fe1
Gabriel Girardi <ggirardii@hotmail.com>
2018-09-16 20:40:37 -0300

    Retirada de redundancia de script

--- Projeto01/scripts/App.js
+++
@@ -1,1 +0,0 @@
-﻿const swal = require('sweetalert2');
---
+++ Projeto01/scripts/MenuBotoes.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    var caminho = window.location.pathname.split('/');
+    for (var i = 0; i < caminho.length; i++) {
+        if (caminho[1] == "") {
+            $('#liPaginaInicial').addClass("active");
+        };
+        if (caminho[i] == "Fabricantes") {
+            $('#liFabricantes').addClass("active");
+        };
+        if (caminho[i] == "Categorias") {
+            $('#liCategorias').addClass("active");
+        };
+        if (caminho[i] == "Carrinho") {
+            $('#liCarrinho').addClass("active");
+        };
+        if (caminho[i] == "Produtos") {
+            $('#liProdutos').addClass("active");
+        };
+        if (caminho[i] == "Admin") {
+            $('#liUsuarios').addClass("active");
+        };
+        if (caminho[i] == "PapelAdmin") {
+            $('#liPapeis').addClass("active");
+        };
+    }
+});
